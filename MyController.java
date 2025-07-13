850f08948d524c4a04082723e31c32c492a4f394
Balu M <balumasavale8@gmail.com>
2025-07-13 22:08:16 +0530

    Add files via upload

---
+++ MyController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/welcome/{name}")
+public String welcome(@PathVariable("name") String name){
+
+return name+ " welcome here";
+}
+}
